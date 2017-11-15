c8bdb8e0f497f059285c5a9892606627b2962070
Bilge <bilge@scriptfusion.com>
2017-11-15 08:15:06 +0000

    Added support for different algorithms and weightings.

--- src/SiteGenerator.php
+++ src/SiteGenerator.php
@@ -5,31 +5,48 @@ namespace ScriptFUSION\Steam250\SiteGenerator;
 
 use Doctrine\DBAL\Connection;
 use Psr\Log\LoggerInterface;
+use ScriptFUSION\Top250\Shared\Algorithm;
 
 final class SiteGenerator
 {
     private $twig;
-
     private $database;
-
     private $logger;
-
     private $outPath;
+    private $algorithm;
+    private $weight;
 
-    public function __construct(\Twig_Environment $twig, Connection $database, LoggerInterface $logger, string $outPath)
-    {
+    public function __construct(
+        \Twig_Environment $twig,
+        Connection $database,
+        LoggerInterface $logger,
+        string $outPath,
+        Algorithm $algorithm,
+        float $weight
+    ) {
         $this->twig = $twig;
         $this->database = $database;
         $this->logger = $logger;
         $this->outPath = $outPath;
+        $this->algorithm = $algorithm;
+        $this->weight = $weight;
     }
 
     public function generate(): void
     {
-        $this->logger->info("Generating site using database: \"{$this->database->getParams()['path']}\".");
+        $this->logger->info(
+            "Generating site using database: \"{$this->database->getParams()['path']}\""
+            . " and \"$this->algorithm\" algorithm ($this->weight)."
+        );
 
-        $cursor = Queries::fetchTop250Games($this->database);
+        $cursor = Queries::fetchTop250Games($this->database, $this->algorithm, $this->weight);
         $games = $cursor->fetchAll();
+
+        if (!$games) {
+            $this->logger->error('No games matching query.');
+
+            return;
+        }
 
         file_put_contents("$this->outPath/index.html", $this->twig->load('250.twig')->render(compact('games')));
 
