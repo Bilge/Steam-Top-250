513611d538f57b90573c7fe41598a455940db8f9
Bilge <bilge@scriptfusion.com>
2017-11-17 03:54:53 +0000

    Rewrote genall using loop.

--- bin/genall.sh
+++ bin/genall.sh
@@ -2,19 +2,29 @@
 
 cd "$(dirname "$0")"/..
 
-db=../Steam\ importer\ test/db/steam80.sqlite
+set -e
 
-bin/generate generate -a wilson -w 1.96 "$db" site/wilson95.html
-bin/generate generate -a wilson -w 2.5759 "$db" site/wilson99.html
-bin/generate generate -a wilson -w 4.4172 "$db" site/wilson99999.html
-bin/generate generate -a bayesian -w 1 "$db" site/bayesian1.html
-bin/generate generate -a bayesian -w 10 "$db" site/bayesian10.html
-bin/generate generate -a bayesian -w 50 "$db" site/bayesian50.html
-bin/generate generate -a bayesian -w 100 "$db" site/bayesian100.html
-bin/generate generate -a bayesian -w 500 "$db" site/bayesian500.html
-bin/generate generate -a bayesian -w 1000 "$db" site/bayesian1000.html
-bin/generate generate -a bayesian -w 5000 "$db" site/bayesian5000.html
-bin/generate generate -a laplace -w 1 "$db" site/laplace1.html
-bin/generate generate -a laplace -w 500 "$db" site/laplace500.html
-bin/generate generate -a laplace_log -w 1 "$db" site/laplace_log1.html
-bin/generate generate -a torn -w 1 "$db" site/torn1.html
+db=../Steam\ importer\ test/db/steam84.sqlite
+
+algos=(
+    'wilson	1.96	95'
+    'wilson	2.5759	99'
+    'wilson	4.4172	99999'
+    'bayesian	1'
+    'bayesian	50'
+    'bayesian	100'
+    'bayesian	500'
+    'bayesian	1000'
+    'bayesian	5000'
+    'laplace	1'
+    'laplace	500'
+    'laplace_log	1'
+    'torn	3.3219'
+)
+
+for algo in "${algos[@]}"; do
+	alg=$(cut -f1 <<<"$algo"); weight=$(cut -f2 <<<"$algo"); alias=$(cut -f3 <<<"$algo")
+
+	../Steam\ importer\ test/bin/250 decorate -a $alg -w $weight "$db"
+	bin/generate generate -a $alg -w $weight "$db" "site/$alg${alias:-$weight}.html"
+done
